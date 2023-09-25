be0ef5bf4dc014167dfcbe8897579bf70c760783
adarsh0701 <adarshmishra1237@gmail.com>
2023-09-25 23:54:46 +0530

    First Commit

---
+++ Backend/app.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const cors=require('cors');
+const app=express()
+
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const server = () => {
+  console.log('You are listening to port: ', PORT);
+};
+server();
