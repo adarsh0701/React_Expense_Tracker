8faa6bd6e56f9c28043cd55fc9d31d1d9355811b
adarsh0701 <adarshmishra1237@gmail.com>
2023-09-27 00:37:06 +0530

    Second Commit(Routes)

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,27 @@
-const express=require('express')
-const cors=require('cors');
-const app=express()
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./db/db');
+const app = express();
+const { readdirSync } = require('fs');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+//routes
+readdirSync('./routes').map((route) =>
+  app.use('/api/v1', require('./routes/' + route))
+);
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World');
+// });
+
 const server = () => {
-  console.log('You are listening to port: ', PORT);
+  db();
+  app.listen(PORT, () => console.log('Listening to port: ', PORT));
 };
 server();
