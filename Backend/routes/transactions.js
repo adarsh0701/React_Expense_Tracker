a18447591ea39e934219130a91139c530fcf4ff4
adarsh0701 <adarshmishra1237@gmail.com>
2023-09-29 23:52:11 +0530

    ThirdCommit

--- Backend/routes/transactions.js
+++ Backend/routes/transactions.js
@@ -1,7 +1,13 @@
-const { addIncome } = require('../controllers/Income');
+const {
+  addIncome,
+  getIncomes,
+  deleteIncome,
+} = require('../controllers/Income');
 
 const router = require('express').Router();
 
-router.post('/add-income', addIncome);
-
+router
+  .post('/add-income', addIncome)
+  .get('/get-incomes', getIncomes)
+  .delete('/delete-income/:id', deleteIncome);
 module.exports = router;
