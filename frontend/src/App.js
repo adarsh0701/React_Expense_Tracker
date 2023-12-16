fab5bd1cef9d1a9f1eed2a1cd243a7468a69e8ae
adarsh0701 <adarshmishra1237@gmail.com>
2023-12-16 23:21:12 +0530

    NavBar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,20 @@ import styled from 'styled-components';
 import bg from './img/bg.png';
 import { MainLayout } from './styles/Layouts';
 import Orb from './components/Orb/orb';
+import Navigation from './components/Navigation/Navigation';
+import { useState } from 'react';
 function App() {
+  const [active, setActive] = useState(1);
+
   return (
     <AppStyled bg={bg} className="App">
       <Orb></Orb>
-      <MainLayout></MainLayout>
+      <MainLayout>
+        <Navigation
+          active={active}
+          setActive={setActive}
+        ></Navigation>
+      </MainLayout>
     </AppStyled>
   );
 }
