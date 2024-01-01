9e59ebc6dda3752d7dfaeea1585251c691be04cd
adarsh0701 <adarshmishra1237@gmail.com>
2024-01-02 01:22:10 +0530

    Added Display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,30 @@
 import styled from 'styled-components';
 import bg from './img/bg.png';
 import { MainLayout } from './styles/Layouts';
-import Orb from './components/Orb/orb';
-import Navigation from './components/Navigation/Navigation';
-import { useState } from 'react';
+import Orb from './Components/Orb/orb';
+import Navigation from './Components/Navigation/Navigation';
+import { useMemo, useState } from 'react';
 function App() {
   const [active, setActive] = useState(1);
 
+  const dispalyData = () => {
+    switch (active) {
+      case 1:
+      //  return <Dashboard />;
+    }
+  };
+  const orbMemo = useMemo(() => {
+    return <Orb />;
+  }, []);
   return (
     <AppStyled bg={bg} className="App">
-      <Orb></Orb>
+      {orbMemo}
       <MainLayout>
         <Navigation
           active={active}
           setActive={setActive}
         ></Navigation>
+        <main>{dispalyData()}</main>
       </MainLayout>
     </AppStyled>
   );
@@ -22,7 +32,20 @@ function App() {
 
 const AppStyled = styled.div`
   height: 100vh;
-  backgound-image: url(${(props) => props.bg});
+  background-image: url(${(props) => props.bg});
   position: relative;
+  main {
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #ffffff;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+
+    overflow: auto;
+    overflow-x: hidden;
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+  }
 `;
 export default App;
