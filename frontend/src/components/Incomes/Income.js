c0a4b3c1a6a50da0c016877ad47247786fe2be4e
adarsh0701 <adarshmishra1237@gmail.com>
2024-01-17 00:06:56 +0530

    Total Income

--- frontend/src/components/Incomes/Income.js
+++ frontend/src/components/Incomes/Income.js
@@ -7,8 +7,13 @@ import IncomeItem from '../IncomeItem/IncomeItem';
 import { de } from 'date-fns/locale';
 
 export default function Income() {
-  const { addIncome, incomes, getIncomes, deleteIncome } =
-    useGlobalContext();
+  const {
+    addIncome,
+    incomes,
+    getIncomes,
+    deleteIncome,
+    totalIncome,
+  } = useGlobalContext();
   useEffect(() => {
     getIncomes();
   }, []);
@@ -16,6 +21,10 @@ export default function Income() {
     <IncomeStyled>
       <InnerLayout>
         <h1>Incomes</h1>
+        <h2 className="total-income">
+          Total Income:
+          <span>${totalIncome()}</span>
+        </h2>
         <div className="income-content">
           <div className="form-container">
             <Form />
