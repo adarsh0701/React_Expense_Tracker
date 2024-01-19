741a2906c36d9e9d591a68c16b3e823fec2b5d77
adarsh0701 <adarshmishra1237@gmail.com>
2024-01-19 23:35:32 +0530

    Added Dashboard

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
 import { InnerLayout } from '../../styles/Layouts';
+import Chart from '../Chart/Chart';
 
 export default function Dashboard() {
   return (
     <DashboardStyled>
-      <InnerLayout></InnerLayout>
+      <InnerLayout>
+        <h1>All Transactions</h1>
+        <div className="stats-con">
+          <div className="chart-con">
+            <Chart />
+          </div>
+        </div>
+      </InnerLayout>
     </DashboardStyled>
   );
 }
 
-const DashboardStyled = styled.div``;
+const DashboardStyled = styled.div`
+  .stats-con {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 2rem;
+    .chart-con {
+      grid-column: 1 / 4;
+      height: 400px;
+      .amount-con {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 2rem;
+        margin-top: 2rem;
+        .income,
+        .expense {
+          grid-column: span 2;
+        }
+        .income,
+        .expense,
+        .balance {
+          background: #fcf6f9;
+          border: 2px solid #ffffff;
+          box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+          border-radius: 20px;
+          padding: 1rem;
+          p {
+            font-size: 3.5rem;
+            font-weight: 700;
+          }
+        }
+      }
+    }
+  }
+`;
